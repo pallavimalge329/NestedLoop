9ffc574b893ea9a6fbae0dd4e9e7d18fa7eff310
HP <HP@DESKTOP-N5QBMVO>
2021-09-01 14:46:49 +0530

    Add project files.

---
+++ NestedLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             for (int i = 1; i <= 5; i++)
+             {
+                 for (int j = 1; j <= i; j++)
+                 {
+                     Console.Write(j);
+                 }
+
+                 Console.WriteLine();
+             }
+             Console.ReadLine();
+
+         //   Console.WriteLine("\n");
+
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
